cd49eee393fad8f8fc7b3f76a0485bcf0c82e243
luckyDaveKim <lucky_dave@naver.com>
2021-08-29 21:51:59 +0900

    feat: Use jdbc

---
+++ src/com/springboot/example/Main.java
@@ -0,0 +1,29 @@
+package com.springboot.example;
+
+import com.springboot.example.dao.UserDao;
+import com.springboot.example.domain.User;
+
+import java.sql.SQLException;
+
+public class Main {
+
+  public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    UserDao dao = new UserDao();
+
+    User user = new User();
+    user.setId("dave");
+    user.setName("김민규");
+    user.setPassword("lucky");
+
+    dao.add(user);
+
+    System.out.println(user.getId() + " 등록 성공");
+
+    User user2 = dao.get(user.getId());
+    System.out.println(user2.getName());
+    System.out.println(user2.getPassword());
+
+    System.out.println(user2.getId() + " 조회 성공");
+  }
+
+}
