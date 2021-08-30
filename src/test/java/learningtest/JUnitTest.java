f0fb7ffc356c54895b65992a9b9ef39881056dc7
luckyDaveKim <lucky_dave@naver.com>
2021-08-30 14:31:53 +0900

    feat: Add context object separation test

---
+++ src/test/java/learningtest/JUnitTest.java
@@ -0,0 +1,59 @@
+package learningtest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/emptyContext.xml")
+public class JUnitTest {
+
+  @Autowired
+  ApplicationContext context;
+
+  static Set<JUnitTest> testObjects = new HashSet<>();
+  static ApplicationContext contextObject = null;
+
+  @Test
+  public void contextObjectSeparationTest1() {
+    assertThat(testObjects).isNotIn(this);
+    testObjects.add(this);
+
+    assertThat(contextObject).satisfiesAnyOf(
+        param -> assertThat(param).isNull(),
+        param -> assertThat(param).isEqualTo(context)
+    );
+    contextObject = this.context;
+  }
+
+  @Test
+  public void contextObjectSeparationTest2() {
+    assertThat(testObjects).isNotIn(this);
+    testObjects.add(this);
+
+    assertThat(contextObject).satisfiesAnyOf(
+        param -> assertThat(param).isNull(),
+        param -> assertThat(param).isEqualTo(context)
+    );
+    contextObject = this.context;
+  }
+
+  @Test
+  public void contextObjectSeparationTest3() {
+    assertThat(testObjects).isNotIn(this);
+    testObjects.add(this);
+
+    assertThat(contextObject).satisfiesAnyOf(
+        param -> assertThat(param).isNull(),
+        param -> assertThat(param).isEqualTo(context)
+    );
+    contextObject = this.context;
+  }
+}
