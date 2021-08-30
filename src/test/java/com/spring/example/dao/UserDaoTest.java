528c42f28c196d2a1c6bf87b89f6591fd28209ec
luckyDaveKim <lucky_dave@naver.com>
2021-08-30 10:19:32 +0900

    feat: Improve get test

--- src/test/java/com/spring/example/dao/UserDaoTest.java
+++ src/test/java/com/spring/example/dao/UserDaoTest.java
@@ -15,20 +15,23 @@ public class UserDaoTest {
     ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
     UserDao dao = context.getBean("userDao", UserDao.class);
 
+    User user1 = new User("dave", "김민규", "lucky");
+    User user2 = new User("dave2", "김민규2", "lucky2");
+
     dao.deleteAll();
     assertThat(dao.getCount()).isEqualTo(0);
 
-    User user = new User();
-    user.setId("dave");
-    user.setName("김민규");
-    user.setPassword("lucky");
+    dao.add(user1);
+    dao.add(user2);
+    assertThat(dao.getCount()).isEqualTo(2);
 
-    dao.add(user);
-    assertThat(dao.getCount()).isEqualTo(1);
+    User foundUser1 = dao.get(user1.getId());
+    assertThat(foundUser1.getName()).isEqualTo(user1.getName());
+    assertThat(foundUser1.getPassword()).isEqualTo(user1.getPassword());
 
-    User user2 = dao.get(user.getId());
-    assertThat(user2.getName()).isEqualTo(user.getName());
-    assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    User foundUser2 = dao.get(user2.getId());
+    assertThat(foundUser2.getName()).isEqualTo(user2.getName());
+    assertThat(foundUser2.getPassword()).isEqualTo(user2.getPassword());
   }
 
   @Test
