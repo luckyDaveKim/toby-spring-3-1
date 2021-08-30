beb6e77589bb3d687e7f1f0976821b6d3157377e
luckyDaveKim <lucky_dave@naver.com>
2021-08-30 17:16:10 +0900

    refactor: Refactor try with resource

--- src/main/java/com/spring/example/dao/UserDao.java
+++ src/main/java/com/spring/example/dao/UserDao.java
@@ -85,67 +85,27 @@ public class UserDao {
   }
 
   public int getCount() throws SQLException {
-    Connection c = null;
-    PreparedStatement ps = null;
-    ResultSet rs = null;
-
-    try {
-      c = dataSource.getConnection();
-
-      ps = c.prepareStatement("select count(*) from users");
-
-      rs = ps.executeQuery();
+    try (
+        Connection c = dataSource.getConnection();
+        PreparedStatement ps = c.prepareStatement(
+            "select count(*) from users");
+        ResultSet rs = ps.executeQuery()
+    ) {
       rs.next();
       return rs.getInt(1);
     } catch (SQLException e) {
       throw e;
-    } finally {
-      if (rs != null) {
-        try {
-          rs.close();
-        } catch (SQLException e) {
-        }
-      }
-      if (ps != null) {
-        try {
-          ps.close();
-        } catch (SQLException e) {
-        }
-      }
-      if (c != null) {
-        try {
-          c.close();
-        } catch (SQLException e) {
-        }
-      }
     }
   }
 
   private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-    Connection c = null;
-    PreparedStatement ps = null;
-
-    try {
-      c = dataSource.getConnection();
-
-      ps = stmt.makePreparedStatement(c);
-
+    try (
+        Connection c = dataSource.getConnection();
+        PreparedStatement ps = stmt.makePreparedStatement(c)
+    ) {
       ps.executeUpdate();
     } catch (SQLException e) {
       throw e;
-    } finally {
-      if (ps != null) {
-        try {
-          ps.close();
-        } catch (SQLException e) {
-        }
-      }
-      if (c != null) {
-        try {
-          c.close();
-        } catch (SQLException e) {
-        }
-      }
     }
   }
 }
