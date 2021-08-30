46ad47e3762996c6eff2b7e9b67494bedf14cc86
luckyDaveKim <lucky_dave@naver.com>
2021-08-30 10:01:50 +0900

    feat: Use JUnit for test

--- src/test/java/com/spring/example/dao/UserDaoTest.java
+++ src/test/java/com/spring/example/dao/UserDaoTest.java
@@ -1,13 +1,17 @@
 package com.spring.example.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.spring.example.domain.User;
 import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
 
-  public static void main(String[] args) throws SQLException {
+  @Test
+  public void addAndGet() throws SQLException {
     ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
     UserDao dao = context.getBean("userDao", UserDao.class);
 
@@ -18,12 +22,8 @@ public class UserDaoTest {
 
     dao.add(user);
 
-    System.out.println(user.getId() + " 등록 성공");
-
     User user2 = dao.get(user.getId());
-    System.out.println(user2.getName());
-    System.out.println(user2.getPassword());
-
-    System.out.println(user2.getId() + " 조회 성공");
+    assertThat(user2.getName()).isEqualTo(user.getName());
+    assertThat(user2.getPassword()).isEqualTo(user.getPassword());
   }
 }
